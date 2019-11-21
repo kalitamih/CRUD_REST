c755a33327e7f66f66a2f7adabc6183c0bd91efb
Mikhail Kalita <Mikhail_Kalita@epam.com>
2019-11-21 18:25:59 +0300

    feat: router get for user id

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,26 @@
 import express from "express";
+import Joi from "joi";
 const app = express();
 const port = 8080;
+
+interface User {
+  login: string;
+  passsword: string;
+  age: number;
+  isDeleted: boolean;
+}
+
+interface ListUsers {
+  [key: string]: User;
+}
+
+const listUsers: ListUsers = {};
+
+app.get("/users/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const user = listUsers.id;
+  res.json({ message: "Resource fetched" });
+});
 
 app.listen(port, () => {
   // tslint:disable-next-line:no-console
