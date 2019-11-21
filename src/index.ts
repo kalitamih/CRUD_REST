5b9ed795edcd4f046c5b662762adba4d1b3f4832
Mikhail Kalita <Mikhail_Kalita@epam.com>
2019-11-21 17:45:38 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import express from "express";
+const app = express();
+const port = 8080;
+
+app.listen(port, () => {
+  // tslint:disable-next-line:no-console
+  console.log(`server started at http://localhost:${port}`);
+});
