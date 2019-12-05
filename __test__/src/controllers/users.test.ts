ceb5b50ec5fdab27c25c55ea834a243b22cbfc44
Mikhail <mkalita1991@gmail.com>
2019-12-05 13:37:18 +0300

    feat: tests user controller

--- __test__/src/controllers/users.test.ts
+++ __test__/src/controllers/users.test.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import {
+  changeUserController,
   createUserController,
+  deleteUserController,
   getUserController,
   getUsersController,
 } from "../../../src/controllers/users";
@@ -8,52 +10,136 @@ import { User } from "../../../src/models/user";
 
 const res = {} as Response;
 
+const next = () => null;
+
 describe("user controller", () => {
-  it("User.findByPk will be invoked 1 time and with id, when id is passed in params", async () => {
-    const req = {
-      params: {
-        id: "123",
-      },
-    } as any;
-    const spyFindByPk = jest.spyOn(User, "findByPk");
-    await getUserController(req, res, () => null);
-    expect(spyFindByPk).toHaveBeenCalledWith("123");
-    expect(spyFindByPk).toHaveBeenCalledTimes(1);
+  describe("getUserController", () => {
+    it("User.findByPk will be invoked 1 time and with id, when id is passed in params", async () => {
+      const req = {
+        params: {
+          id: "123",
+        },
+      } as any;
+      const spyFindByPk = jest.spyOn(User, "findByPk");
+      await getUserController(req, res, next);
+      expect(spyFindByPk).toHaveBeenCalledWith("123");
+      expect(spyFindByPk).toHaveBeenCalledTimes(1);
+      spyFindByPk.mockClear();
+    });
   });
-  it("User.findByAll will be invoked 1 time and with right parametres, when loginSubstring and limit are passed in query", async () => {
-    const req = {
-      query: {
-        limit: 3,
-        loginSubstring: "M",
-      },
-    } as any;
-    const spyFindAll = jest.spyOn(User, "findAll") as any;
-    await getUsersController(req, res, () => null);
-    const {
-      limit,
-      order,
-      attributes: { exclude },
-    } = spyFindAll.mock.calls[0][0];
-    expect(limit).toBe(3);
-    expect(order).toStrictEqual([["login", "ASC"]]);
-    expect(exclude).toStrictEqual(["isDeleted"]);
-    expect(spyFindAll).toHaveBeenCalledTimes(1);
+
+  describe("getUsersController", () => {
+    it("User.findByAll will be invoked 1 time and with right parametres, when loginSubstring and limit are passed in query", async () => {
+      const req = {
+        query: {
+          limit: 3,
+          loginSubstring: "M",
+        },
+      } as any;
+      const spyFindAll = jest.spyOn(User, "findAll") as any;
+      await getUsersController(req, res, () => null);
+      const {
+        limit,
+        order,
+        attributes: { exclude },
+      } = spyFindAll.mock.calls[0][0];
+      expect(limit).toBe(3);
+      expect(order).toStrictEqual([["login", "ASC"]]);
+      expect(exclude).toStrictEqual(["isDeleted"]);
+      expect(spyFindAll).toHaveBeenCalledTimes(1);
+      spyFindAll.mockClear();
+    });
   });
-  it("User.findOne and User.create will be invoked, when login, age, password are passed in body", async () => {
+
+  describe("createUserController", () => {
+    const user = {
+      age: 27,
+      login: "kalitamih",
+      password: "M1991M",
+    };
     const req = {
-      body: {
-        age: 27,
-        login: "kalitamih",
-        password: "M1991M",
-      },
+      body: user,
     } as any;
-    const spyFindOne = jest.spyOn(User, "findOne") as any;
-    const spyCreate = jest.spyOn(User, "create") as any;
-    spyFindOne.mockReturnValueOnce({
-      promise: () => Promise.resolve(null),
-    } as any);
-    await createUserController(req, res, () => null);
-    expect(spyFindOne).toHaveBeenCalledTimes(1);
-    expect(spyCreate).toHaveBeenCalledTimes(1);
+
+    it("User.create is invoked, when User.findOne returns null", async () => {
+      const spyFindOne = jest.spyOn(User, "findOne") as any;
+      const spyCreate = jest.spyOn(User, "create") as any;
+      spyFindOne.mockReturnValueOnce(Promise.resolve(null));
+      await createUserController(req, res, next);
+      expect(spyFindOne).toHaveBeenCalledWith({
+        where: { login: "kalitamih" },
+      });
+      expect(spyFindOne).toHaveBeenCalledTimes(1);
+      expect(spyCreate).toHaveBeenCalledWith(user);
+      expect(spyCreate).toHaveBeenCalledTimes(1);
+      spyFindOne.mockClear();
+      spyCreate.mockClear();
+    });
+
+    it("User.create is not invoked, when User.findOne returns user's object", async () => {
+      const spyFindOne = jest.spyOn(User, "findOne") as any;
+      const spyCreate = jest.spyOn(User, "create") as any;
+      spyFindOne.mockReturnValueOnce(Promise.resolve({ id: 1, ...user }));
+      await createUserController(req, res, next);
+      expect(spyFindOne).toHaveBeenCalledWith({
+        where: { login: "kalitamih" },
+      });
+      expect(spyFindOne).toHaveBeenCalledTimes(1);
+      expect(spyCreate).toHaveBeenCalledTimes(0);
+      spyFindOne.mockClear();
+      spyCreate.mockClear();
+    });
+  });
+
+  describe("deleteUserController", () => {
+    it("User.update is invoked with right params, when when id is passed in params", async () => {
+      const req = {
+        params: {
+          id: "123",
+        },
+      } as any;
+      const spyUpdate = jest.spyOn(User, "update") as any;
+      await deleteUserController(req, res, next);
+      expect(spyUpdate).toHaveBeenCalledWith(
+        { isDeleted: true },
+        {
+          returning: true,
+          where: {
+            id: "123",
+            isDeleted: false,
+          },
+        }
+      );
+      expect(spyUpdate).toHaveBeenCalledTimes(1);
+      spyUpdate.mockClear();
+    });
+  });
+
+  describe("changeUserController", () => {
+    it("User.update is invoked with right params, when when id is passed in params", async () => {
+      const req = {
+        body: {
+          age: 27,
+          password: "M1991M",
+        },
+        params: {
+          id: "123",
+        },
+      } as any;
+      const spyUpdate = jest.spyOn(User, "update") as any;
+      await changeUserController(req, res, next);
+      expect(spyUpdate).toHaveBeenCalledWith(
+        { age: 27, password: "M1991M" },
+        {
+          returning: true,
+          where: {
+            id: "123",
+            isDeleted: false,
+          },
+        }
+      );
+      expect(spyUpdate).toHaveBeenCalledTimes(1);
+      spyUpdate.mockClear();
+    });
   });
 });
